3319988942961418013482a21558fefc01518624
humaid7madi <crashbashpc@gmail.com>
2023-06-22 20:03:50 +0400

    commit

---
+++ ROS/workspace/cartking_ws/build/simple_arm/CMakeFiles/arm_mover.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/workspace/cartking_ws/devel/lib/simple_arm/arm_mover"
+  "/home/workspace/cartking_ws/devel/lib/simple_arm/arm_mover.pdb"
+  "CMakeFiles/arm_mover.dir/src/arm_mover.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arm_mover.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
