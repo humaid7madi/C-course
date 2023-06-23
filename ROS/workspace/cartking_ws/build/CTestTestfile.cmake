030dd09256eceff9d79bb05162740f8e915b0a28
humaid7madi <94049507+humaid7madi@users.noreply.github.com>
2023-06-23 10:29:04 +0400

    Delete ROS/workspace/cartking_ws directory

--- ROS/workspace/cartking_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/cartking_ws/src
-# Build directory: /home/workspace/cartking_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("first_package")
-subdirs("simple_arm")
--- ROS/workspace/cartking_ws/build/first_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/cartking_ws/src/first_package
-# Build directory: /home/workspace/cartking_ws/build/first_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
